b21e2904a3ae6c7c41db674a82c70a6f50a38baf
Ari Supriatna <ari@myboost.id>
2021-02-06 22:46:41 +0700

    Initial Commit

---
+++ Rilabs UITesting/Rilabs_UITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rilabs_UITestingApp.swift
+//  Rilabs UITesting
+//
+//  Created by Ari Supriatna on 06/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Rilabs_UITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
